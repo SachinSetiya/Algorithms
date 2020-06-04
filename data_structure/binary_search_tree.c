1e4eef03f088f6226ddb4ddd3b3ec89d2a15de65
Sachin <sachin.setiya@mariadb.com>
2020-06-04 21:29:02 +0530

    Chnage Data Structure name

---
+++ data_structure/binary_search_tree.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "include/random_generator.h"
+int main()
+{
+  printf("Sachin using VSCODE");
+  check();
+}
---
+++ include/random_generator.h
@@ -0,0 +1,10 @@
+//#ifndef RANDOM_GENERATOR_H
+#define RANDOM_GENERATOR_H
+#include<stdio.h>
+
+void check()
+{
+  printf("I am here");
+}
+
+//#endif //RANDOM_GENERATOR_H
